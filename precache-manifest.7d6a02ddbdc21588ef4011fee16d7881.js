99d2acf50f82dd64946cbe85c9dce1fe195bac8d
Julie Mathews <juliem.forever@gmail.com>
2019-05-06 19:55:33 -0700

    Updates

---
+++ precache-manifest.7d6a02ddbdc21588ef4011fee16d7881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859134e6b861c697e6a8a1fc7e02c609",
+    "url": "/clickclick/index.html"
+  },
+  {
+    "revision": "619bb7ce51ef515e3ff6",
+    "url": "/clickclick/static/css/main.559fb59e.chunk.css"
+  },
+  {
+    "revision": "48bb99afeae4b3f345e6",
+    "url": "/clickclick/static/js/2.3c1fd096.chunk.js"
+  },
+  {
+    "revision": "619bb7ce51ef515e3ff6",
+    "url": "/clickclick/static/js/main.ef789e24.chunk.js"
+  },
+  {
+    "revision": "070ddd0a22c9ecb8c14b",
+    "url": "/clickclick/static/js/runtime~main.677d155b.js"
+  }
+]);
